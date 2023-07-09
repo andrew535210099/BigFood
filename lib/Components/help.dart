a47ec6fbf0aa919f05674771668c36f57ec3c739
PeterJames16 <peter.535210096@stu.untar.ac.id>
2023-07-09 12:53:34 +0700

    help

--- lib/Components/help.dart
+++ lib/Components/help.dart
@@ -5,14 +5,13 @@ import 'package:duds/Components/orderdetails.dart';
 import 'package:flutter/material.dart';
 import 'package:duds/Components/uploadpreview_screen.dart';
 
-
 import 'package:duds/Components/rooms.dart';
+
 void main() {
   runApp(HelpPage());
 }
 
 class HelpPage extends StatefulWidget {
-
   @override
   State<HelpPage> createState() => _HelpPageState();
 }
@@ -21,29 +20,26 @@ class _HelpPageState extends State<HelpPage> {
   late int currentIndex;
   void initState() {
     super.initState();
-
   }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-    backgroundColor: const Color.fromARGB(255, 255, 100, 64),
-      title: const Text('FAQ'),
-        leading: IconButton(
-    icon: Icon(Icons.arrow_back),
-    onPressed: () {
-        Navigator.pushNamed(context, '/homebar');// Kembali ke halaman sebelumnya
-    },
-  )),
-    body:const Steps(), 
-  );
-
+        appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 255, 100, 64),
+            title: const Text('FAQ'),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pushNamed(
+                    context, '/homebar'); // Kembali ke halaman sebelumnya
+              },
+            )),
+        body: const Steps(),
+      );
 }
+
 class Step {
-  Step(
-    this.title,
-    this.body,
-    [this.isExpanded = false]
-  );
+  Step(this.title, this.body, [this.isExpanded = false]);
   String title;
   String body;
   bool isExpanded;
@@ -51,10 +47,43 @@ class Step {
 
 List<Step> getSteps() {
   return [
-    Step('Step 0: Install Flutter', 'Install Flutter development tools according to the official documentation.'),
-    Step('Step 1: Create a project', 'Open your terminal, run `flutter create <project_name>` to create a new project.'),
-    Step('Step 2: Run the app', 'Change your terminal directory to the project directory, enter `flutter run`.'),
-   Step('Step 2: Run the app', 'Change your terminal directory to the project directory, enter `flutter run`.'),
+    Step('Mengapa saya tidak bisa membuat pesanan di checkout?', 'answer'),
+    Step('Mengapa saya tidak bisa login ke akun BigFood saya?',
+        '1. Pastikan password dan email yang dimasukan sudah benar, 2. Apakah anda sudah 5 kali berturut-turut salah password? jika ya, akun ada sudah terblokir sehingga anda tidak dapat login ke akun BigFood anda, 3. Jika lupa Password, anda bisa melakukan `reset password`'),
+    Step('Bagaimana cara memperbarui profile saya?',
+        'Untuk memperbarui profile, anda bisa klik `ProfilePage` di kanan bawah'),
+    Step('Dapatkah akun BigFood saya masuk di beberapa perangkat?',
+        'Ya, akun BigFood anda dapat masuk dan digunakan di beberapa perangkat'),
+    Step('Bagaimana cara menyimpan pesanan di keranjang',
+        'Anda dapat membuka halaman `home` kami lalu mengklik tanda `+` untuk menambah menu lalu `-` untuk menurangi menu'),
+    Step('Bagaimana cara mengatur ulang akun BigFood saya jika lupa password?',
+        'Anda dapat melakukan `reset password` dihalaman login'),
+    Step('Apakah saya dapat mengubah metode pembayaran?',
+        'Ya bisa, anda dapat mengubah metode pembayaran pada saat checkout'),
+    Step('Bagaimana cara menghapus akun BigFood?',
+        '1. Anda dapat membuka halaman `ProfilePage`, 2. Setelah itu anda dapat klik `Delete Account` untuk menghapus akun BigFood anda'),
+    Step('Bisakah saya mengganti menu setelah pemesanan terbuat?',
+        'Tidak, anda tidak dapat mengganti menu saat pesanan terbuat. Oleh sebab itu perhatikan menu dan jumlah sebelum anda checkout'),
+    Step('Dimana saya melapor jika terjadi kendala dalam hal pembayaran?',
+        'Untuk kendala pembayaran, anda bisa menghubungi bigfood@gmail.com atau hubungi cs kami 08...'),
+    Step(
+        'Aplikasi tidak dapat terbuka, tiba tiba tertutup atau muncul kode error',
+        ''),
+    Step('Saya tidak mendapatkan email untuk reset password',
+        '1. Pastikan email anda benar, 2. Anda bisa `refresh` halaman email anda, 3. Anda juga bisa cek halaman `all mail` di email anda'),
+    Step('Akun saya terblokir',
+        'Akun anda akan terblokir apabila anda salah memasukan password sebanyak 5 kali berturut-turut saat melakukan login'),
+    Step('Pendaftaran BigFood saya bermasalah',
+        'Pastikan Username dan Email anda belum pernah terpakai'),
+    Step('Bisakah saya menghapus riwayat pemesanan saya?',
+        'Tidak, anda tidak dapat menghapus riwayat pemesanan anda'),
+    Step('Bagaimana cara mengajukan keluhan?',
+        'ini nanti kek buat email aja, misal bigfood@gmail.com, jadi lapor kesini'),
+    Step('Lapor biaya tambahan', 'jadi nanti kek gini hasilnya, diline bawah'),
+    Step('Lapor kendala pembayaran',
+        'Untuk kendala pembayaran, anda bisa menghubungi bigfood@gmail.com atau hubungi cs kami 08...'),
+    Step('Bagaimana cara melakukan check out pesanan saya?',
+        'Untuk melakukan checkout pesanan, anda dapat klik halaman `checkou` pada aplikasi kami, lalu anda bisa melakukan pembayaran untuk checkout'),
   ];
 }
 
@@ -74,6 +103,7 @@ class _StepsState extends State<Steps> {
       ),
     );
   }
+
   Widget _renderSteps() {
     return ExpansionPanelList(
       expansionCallback: (int index, bool isExpanded) {
