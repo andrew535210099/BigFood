366115273bf5217e82e3bc9575695f0203a28e79
andrew535210099 <andrew.535210099@stu.untar.ac.id>
2023-07-09 20:38:48 +0700

    Finishing no bug

--- lib/Components/yourorders_screen.dart
+++ lib/Components/yourorders_screen.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class yourOrders extends StatelessWidget {
-  const yourOrders({super.key});
+  const yourOrders({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +21,6 @@ class yourOrders extends StatelessWidget {
     );
   }
 }
-
-
 
 class OrderItem {
   final String title;
@@ -42,72 +40,72 @@ class OrderListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return Row(
-    children: [
-      Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey,
-            width: 1.0,
+    return Row(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey,
+              width: 1.0,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
           ),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: ClipRRect(
-  borderRadius: BorderRadius.circular(10.0),
-  child: item.image != null ? Image(
-    image: AssetImage(item.image),
-    fit: BoxFit.cover,
-  ) : null,
-),
-      ),
-      const SizedBox(
-        width: 20.0,
-      ),
-      Expanded(
-        flex: 2,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Completed",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 0, 0),
-              ),
-            ),
-            const SizedBox(height: 5.0),
-            Text(
-              "OrderID : 1452547",
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Color.fromARGB(255, 0, 0, 0),
-              ),
-            ),
-          ],
-        ),
-      ),
-      const SizedBox(width: 10.0),
-      Container(
-        padding: EdgeInsets.only(right: 20.0), // Atur jarak tepi kanan di sini
-        child: Text(
-          "Rp ${item.price * item.qty}",
-          style: TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-            color: Color.fromARGB(255, 0, 0, 0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: item.image != null
+                ? Image(
+                    image: AssetImage(item.image),
+                    fit: BoxFit.cover,
+                  )
+                : null,
           ),
         ),
-      ),
-    ],
-  );
+        const SizedBox(
+          width: 20.0,
+        ),
+        Expanded(
+          flex: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Completed",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                "OrderID : 1452547",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Container(
+          padding: EdgeInsets.only(right: 20.0), // Atur jarak tepi kanan di sini
+          child: Text(
+            "Rp ${item.price * item.qty}",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
-
-}
-
 
 class OrderDetaill extends StatefulWidget {
   @override
@@ -128,23 +126,30 @@ class _OrderDetaillState extends State<OrderDetaill> {
     final User? currentUser = auth.currentUser;
     final String? userId = currentUser?.uid;
 
-    CollectionReference cartCollection = FirebaseFirestore.instance.collection('users');
-    CollectionReference cartsCollection = FirebaseFirestore.instance.collection('carts');
+    CollectionReference cartCollection =
+        FirebaseFirestore.instance.collection('users');
+    CollectionReference cartsCollection =
+        FirebaseFirestore.instance.collection('carts');
     DocumentSnapshot documentSnapshot = await cartCollection.doc(userId).get();
     String? userEmail = documentSnapshot['email'] as String?;
 
-    QuerySnapshot snapshot = await cartsCollection.where('email', isEqualTo: userEmail).get();
+    QuerySnapshot snapshot =
+        await cartsCollection.where('email', isEqualTo: userEmail).get();
 
     for (DocumentSnapshot doc in snapshot.docs) {
-      Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+      Map<String, dynamic>? data =
+          doc.data() as Map<String, dynamic>?;
 
       if (data != null) {
-        int zingerBurgerQuantity = data['zingerBurgerQuantity'] as int;
-        int rollParathaQuantity = data['rollParathaQuantity'] as int;
+        int zingerBurgerQuantity =
+            data['zingerBurgerQuantity'] as int;
+        int rollParathaQuantity =
+            data['rollParathaQuantity'] as int;
         int burgerQuantity = data['burgerQuantity'] as int;
         int sandwichQuantity = data['sandwichQuantity'] as int;
         int pizzaRollQuantity = data['pizzaRollQuantity'] as int;
-        int mushroomSoupQuantity = data['mushroomSoupQuantity'] as int;
+        int mushroomSoupQuantity =
+            data['mushroomSoupQuantity'] as int;
 
         OrderItem orderItem = OrderItem(
           "Zinger Burger",
@@ -190,8 +195,6 @@ class _OrderDetaillState extends State<OrderDetaill> {
           orderItems.add(orderItem3);
           orderItems.add(orderItem4);
           orderItems.add(orderItem5);
-          
-
         });
       } else {
         print('Properti tidak ditemukan di dokumen dengan ID ${doc.id}');
@@ -201,46 +204,51 @@ class _OrderDetaillState extends State<OrderDetaill> {
 
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final userProvider =
+        Provider.of<UserProvider>(context, listen: false);
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User? currentUser = auth.currentUser;
     final String? userId = currentUser?.uid;
 
     return StreamBuilder<DocumentSnapshot>(
-      stream: FirebaseFirestore.instance.collection('users').doc(userId).snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return Scaffold(
-          appBar: AppBar(
-            backgroundColor: const Color.fromARGB(255, 255, 100, 64),
-            title: const Text('Your Orders'),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
- Navigator.push(
-      context,
-      new MaterialPageRoute(
-        builder: (context) => new OrderDetail(),
-      ),
-    );
-              },
-            )),
-
-            body: Stack(
-              children: [
-                Positioned.fill(
-                  child: Container(
-                    color: Colors.grey.shade200, // Ubah warna latar belakang sesuai kebutuhan
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 255, 100, 64),
+              title: const Text('Your Orders'),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => OrderDetail(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.grey.shade200,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: SingleChildScrollView(
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
                     child: Column(
                       children: [
                         const SizedBox(height: 20.0),
                         ListView.separated(
                           shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
                           itemCount: orderItems.length,
                           itemBuilder: (context, index) {
                             if (orderItems[index].qty == 0) {
@@ -254,12 +262,11 @@ class _OrderDetaillState extends State<OrderDetaill> {
                         ),
                         const SizedBox(height: 10.0),
                         const SizedBox(height: 10.0),
-                        
                       ],
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         } else if (snapshot.hasError) {
@@ -270,28 +277,15 @@ class _OrderDetaillState extends State<OrderDetaill> {
       },
     );
   }
-
-  Container _buildDivider() {
-    return Container(
-      height: 2.0,
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-        color: Colors.grey.shade400,
-        borderRadius: BorderRadius.circular(1.0),
-      ),
-    );
-  }
-  
-  double calculateTotalPrice() {
-  double totalPrice = 0.0;
-  for (var item in orderItems) {
-    totalPrice += (item.qty * item.price);
-  }
-  return totalPrice;
 }
-double calculateOngkir() {
-  double totalPrice = 9000;
 
-  return totalPrice;
-}
+Container _buildDivider() {
+  return Container(
+    height: 2.0,
+    width: double.maxFinite,
+    decoration: BoxDecoration(
+      color: Colors.grey.shade400,
+      borderRadius: BorderRadius.circular(1.0),
+    ),
+  );
 }
